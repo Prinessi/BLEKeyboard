439324b433d353de9bceb19f0ef9ec6c05e6f34f
Nathan Hohenadel <nathanhohenadel1@gmail.com>
2025-05-03 22:00:37 -0400

    Fully fixed the right half.

--- BLELeft/BLELeft.ino
+++ BLELeft/BLELeft.ino
@@ -1,13 +1,14 @@
 #include <BleKeyboard.h>
-#define KEY_PRINT_SCREEN 0x46
 #define KEY_DELETE 0x4C
 #define KEY_F13 0x68
 #define KEY_MEDIA_MUTE 0x7F
 #define KEY_MEDIA_VOLUME_DOWN 0x81
 #define KEY_MEDIA_VOLUME_UP 0x80
 #define KEY_CAPS_LOCK 0x39
+#define KEY_DEVICE_SWITCH KEY_F13  // or any custom unused key
 
-BleKeyboard bleKeyboard("MyBLEKeyboard", "Nathan", 100);
+BleKeyboard bleKeyboard("Splitto Left", "Nathan", 100);
+bool switchDeviceRequested = false;
 
 const int ROWS = 7;
 const int COLS = 8;
@@ -16,26 +17,14 @@ const int rowPins[ROWS] = {15, 2, 4, 16, 17, 5, 18};
 const int colPins[COLS] = {13, 12, 14, 27, 26, 25, 33, 32};
 
 uint8_t keymap[ROWS][COLS] = {
-  {KEY_ESC, KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5, KEY_F6, 0},
+  {KEY_ESC, KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5, KEY_F6, KEY_F13},
   {'`', '1', '2', '3', '4', '5', '-', 0},
   {KEY_TAB, 'q', 'w', 'e', 'r', 't', '[', 0},
   {KEY_LEFT_CTRL, 'a', 's', 'd', 'f', 'g', 0, 0},
   {KEY_LEFT_SHIFT, 'z', 'x', 'c', 'v', 'b', 0, 0},
-  {KEY_LEFT_GUI, KEY_LEFT_ALT, KEY_PRINT_SCREEN, KEY_DELETE, KEY_F13, KEY_MEDIA_MUTE, KEY_MEDIA_VOLUME_DOWN, KEY_MEDIA_VOLUME_UP},
+  {KEY_LEFT_GUI, KEY_LEFT_ALT, KEY_DELETE, 0, KEY_PRTSC, KEY_MEDIA_MUTE, KEY_MEDIA_VOLUME_DOWN, KEY_MEDIA_VOLUME_UP},
   {0, 0, 0, 0, 0, ' ', KEY_RETURN, KEY_CAPS_LOCK}
 };
-
-
-// const int ROWS = 2;
-// const int COLS = 2;
-
-// const int rowPins[ROWS] = {2, 3};
-// const int colPins[COLS] = {4, 5};
-
-// uint8_t keymap[ROWS][COLS] = {
-//   {'a', 'b'},
-//   {'c', 'd'}
-// };
 
 bool keyState[ROWS][COLS];
 bool lastKeyState[ROWS][COLS];
@@ -61,10 +50,18 @@ unsigned long lastKeyTime = 0;
 void loop() {
   if (bleKeyboard.isConnected()) {
     unsigned long now = millis();
-    if (now - lastKeyTime > 20) { // scan/send every 20ms
+    if (now - lastKeyTime > 20) {
       scanMatrix();
       sendKeys();
       lastKeyTime = now;
+    }
+
+    if (switchDeviceRequested) {
+      Serial.println("Switching Bluetooth device...");
+      bleKeyboard.end(); // Disconnect
+      delay(1000);
+      bleKeyboard.begin(); // Restart BLE advertising
+      switchDeviceRequested = false;
     }
   }
 }
@@ -83,7 +80,9 @@ void sendKeys() {
   for (int r = 0; r < ROWS; r++) {
     for (int c = 0; c < COLS; c++) {
       if (keyState[r][c] && !lastKeyState[r][c]) {
-        if (keymap[r][c] != 0) {
+        if (keymap[r][c] == KEY_DEVICE_SWITCH) {
+          switchDeviceRequested = true;
+        } else if (keymap[r][c] != 0) {
           bleKeyboard.press(keymap[r][c]);
         }
       } else if (!keyState[r][c] && lastKeyState[r][c]) {
--- BLERight/BLERight.ino
+++ BLERight/BLERight.ino
@@ -1,13 +1,21 @@
 #include <BleKeyboard.h>
 #define KEY_PRINT_SCREEN 0x46
 #define KEY_DELETE 0x4C
-#define KEY_F13 0x68
-#define KEY_MEDIA_MUTE 0x7F
-#define KEY_MEDIA_VOLUME_DOWN 0x81
-#define KEY_MEDIA_VOLUME_UP 0x80
 #define KEY_CAPS_LOCK 0x39
+#define KEY_DEVICE_SWITCH KEY_F13  // or any custom unused key
+#define KEY_SEMICOLON       0x33
+#define KEY_SHIFT           0x85  // Left Shift
+#define KEY_ARROW_LEFT      0xD8
+#define KEY_ARROW_DOWN      0xD9
+#define KEY_ARROW_UP        0xDA
+#define KEY_ARROW_RIGHT     0xD7
+#define KEY_BACKSLASH   0x31
+#define KEY_MEDIA_PLAY     0xCD
+#define KEY_MEDIA_NEXT     0xB5
+#define KEY_MEDIA_PREVIOUS 0xB6
 
-BleKeyboard bleKeyboard("MyBLEKeyboard", "Nathan", 100);
+BleKeyboard bleKeyboard("Splitto Right", "Nathan", 100);
+bool switchDeviceRequested = false;
 
 const int ROWS = 7;
 const int COLS = 8;
@@ -16,26 +24,14 @@ const int rowPins[ROWS] = {15, 2, 4, 16, 17, 5, 18};
 const int colPins[COLS] = {13, 12, 14, 27, 26, 25, 33, 32};
 
 uint8_t keymap[ROWS][COLS] = {
-  {0, KEY_F7, KEY_F8, KEY_F9, KEY_F10, KEY_F11, KEY_F12, 0},
-  {0, '=', '6', '7', '8', '9', '0', '\'},
-  {0, 'y', 'u', 'i', 'o', 'p', 't',  KEY_BACKSPACE},
-  {0, 'h', 'j', 'k', 'l', ';', ''', KEY_RETURN},
+  {0, KEY_F7, KEY_F8, KEY_F9, KEY_F10, KEY_F11, KEY_F12, KEY_DELETE},
+  {0, '=', '6', '7', '8', '9', '0', '\\'},
+  {0, ']', 'y', 'u', 'i', 'o', 'p',  KEY_BACKSPACE},
+  {0, 0, 'h', 'j', 'k', 'l', ';', '\''},
   {0, 0, 'n', 'm', ',', '.', '/', KEY_SHIFT},
   {KEY_MEDIA_PLAY, KEY_MEDIA_PREVIOUS, KEY_MEDIA_NEXT, KEY_ARROW_LEFT, KEY_ARROW_DOWN, KEY_ARROW_UP, KEY_ARROW_RIGHT, 0},
   {KEY_PAGE_DOWN, KEY_PAGE_UP, KEY_BACKSPACE ,0, 0, 0, 0, 0}
 };
-
-
-// const int ROWS = 2;
-// const int COLS = 2;
-
-// const int rowPins[ROWS] = {2, 3};
-// const int colPins[COLS] = {4, 5};
-
-// uint8_t keymap[ROWS][COLS] = {
-//   {'a', 'b'},
-//   {'c', 'd'}
-// };
 
 bool keyState[ROWS][COLS];
 bool lastKeyState[ROWS][COLS];
@@ -61,13 +57,14 @@ unsigned long lastKeyTime = 0;
 void loop() {
   if (bleKeyboard.isConnected()) {
     unsigned long now = millis();
-    if (now - lastKeyTime > 20) { // scan/send every 20ms
+    if (now - lastKeyTime > 20) {
       scanMatrix();
       sendKeys();
       lastKeyTime = now;
     }
   }
 }
+
 
 void scanMatrix() {
   for (int r = 0; r < ROWS; r++) {
@@ -83,7 +80,9 @@ void sendKeys() {
   for (int r = 0; r < ROWS; r++) {
     for (int c = 0; c < COLS; c++) {
       if (keyState[r][c] && !lastKeyState[r][c]) {
-        if (keymap[r][c] != 0) {
+        if (keymap[r][c] == KEY_DEVICE_SWITCH) {
+          switchDeviceRequested = true;
+        } else if (keymap[r][c] != 0) {
           bleKeyboard.press(keymap[r][c]);
         }
       } else if (!keyState[r][c] && lastKeyState[r][c]) {
